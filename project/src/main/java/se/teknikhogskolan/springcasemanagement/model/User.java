22ca36f5c1e1d43fbfe582eb2985f12c479f303d
Johan Åsling <johan.asling@hotmail.com>
2016-11-01 15:03:22 +0100

    User entity created

--- project/src/main/java/se/teknikhogskolan/springcasemanagement/model/User.java
+++ project/src/main/java/se/teknikhogskolan/springcasemanagement/model/User.java
@@ -1,6 +1,63 @@
 package se.teknikhogskolan.springcasemanagement.model;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class User {
-    private String firstName;
+
     private String username;
+    private String firstName;
+    private String lastName;
+    @ManyToOne
+    private Team team;
+    private boolean active;
+
+    protected User() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+    
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void activate() {
+        active = true;
+    }
+
+    public void inactiviate() {
+        active = false;
+    }
+
 }
