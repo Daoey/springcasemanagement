da94f3bf60b09219e776a00cd1d7cdab19378efa
Martin Söderstrand <martinsoderstrand96@hotmail.com>
2016-11-02 10:08:06 +0100

    Foundation of test classes for the Issue and Team entity added.

---
+++ project/src/test/java/se/teknikhogskolan/springcasemanagement/model/TestTeam.java
@@ -0,0 +1,13 @@
+package se.teknikhogskolan.springcasemanagement.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public final class TestTeam {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+}
---
+++ project/src/test/java/se/teknikhogskolan/springcasemanagement/repository/TestTeamRepository.java
@@ -0,0 +1,13 @@
+package se.teknikhogskolan.springcasemanagement.repository;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public final class TestTeamRepository {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+}
