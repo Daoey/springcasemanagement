30c07f5d348f03577d0c66b87a179305022de3ac
Johan Åsling <johan.asling@hotmail.com>
2016-11-03 11:14:13 +0100

    Wrote some more user repository tests

--- project/src/test/java/se/teknikhogskolan/springcasemanagement/repository/TestUserRepository.java
+++ project/src/test/java/se/teknikhogskolan/springcasemanagement/repository/TestUserRepository.java
@@ -2,10 +2,10 @@ package se.teknikhogskolan.springcasemanagement.repository;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
+import org.junit.After;
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -17,6 +17,11 @@ public final class TestUserRepository {
 
     private User user = new User(1000L, "Long enough name", "First", "Last", null);
 
+    @After
+    public void cleanTest() {
+        removeAllUsers();
+    }
+
     @Test
     public void canSaveAndDeleteUsers() {
 
@@ -27,15 +32,61 @@ public final class TestUserRepository {
 
         users = executeMultiple(userRepository -> userRepository.findAll());
         long numberOfUsersAfterSave = users.spliterator().getExactSizeIfKnown();
-        
+
         assertEquals(numberOfUsersBeforeSave, numberOfUsersAfterSave - 1);
 
         executeVoid(userRepository -> userRepository.delete(user));
-        
+
         users = executeMultiple(userRepository -> userRepository.findAll());
         long numberOfUsersAfterDelete = users.spliterator().getExactSizeIfKnown();
-        
+
         assertEquals(numberOfUsersBeforeSave, numberOfUsersAfterDelete);
+    }
+
+    @Test
+    public void canGetUser() {
+        executeVoid(userRepository -> userRepository.save(user));
+        User userFromDatabase = execute(userRepository -> userRepository.findOne(user.getId()));
+        assertEquals(user, userFromDatabase);
+    }
+
+    @Test
+    public void canSearchForUsers() {
+
+        long numberOfUsersToAdd = 5;
+        for (long i = 0; i < numberOfUsersToAdd; i++) {
+            Long index = i;
+            executeVoid(userRepository -> userRepository.save(new User(user.getUserNumber() + index,
+                    user.getUsername() + index, user.getFirstName(), user.getLastName(), null)));
+        }
+
+        // Adding a similar user that should not be found
+        executeVoid(
+                userRepository -> userRepository.save(new User(100L, "username wrong", "firstNam", "llastname", null)));
+
+        String firstName = user.getFirstName();
+        String lastName = user.getLastName().substring(0, 2);
+        String username = user.getUsername().substring(5, 9);
+
+        Iterable<User> users = executeMultiple(userRepository -> userRepository
+                .findByFirstNameContainingAndLastNameContainingAndUsernameContaining(firstName, lastName, username));
+
+        long numberOfUsersFound = users.spliterator().getExactSizeIfKnown();
+        assertEquals(numberOfUsersToAdd, numberOfUsersFound);
+    }
+
+    @Test
+    public void canFindUserByUserNumber() {
+        executeVoid(userRepository -> userRepository.save(user));
+        User userFromDatabase = execute(userRepository -> userRepository.findByUserNumber(user.getUserNumber()));
+        assertEquals(user, userFromDatabase);
+    }
+
+    private void removeAllUsers() {
+        Iterable<User> users = executeMultiple(userRepository -> userRepository.findAll());
+        for (User user : users) {
+            executeVoid(userRepository -> userRepository.delete(user));
+        }
     }
 
     private Iterable<User> executeMultiple(Function<UserRepository, Iterable<User>> operation) {
