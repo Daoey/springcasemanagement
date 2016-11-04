333d57bf21eeef53fb9db5b14073a1e1bc9fb9c7
Johan Åsling <johan.asling@hotmail.com>
2016-11-04 14:28:44 +0100

    Fixed checkstyle complaints

--- project/src/main/java/se/teknikhogskolan/springcasemanagement/repository/UserRepository.java
+++ project/src/main/java/se/teknikhogskolan/springcasemanagement/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package se.teknikhogskolan.springcasemanagement.repository;
 
-import java.util.Collection;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
@@ -8,9 +7,9 @@ import org.springframework.data.repository.CrudRepository;
 import se.teknikhogskolan.springcasemanagement.model.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-	
+
     User findByUserNumber(Long userNumber);
-    
+
     List<User> findByFirstNameContainingAndLastNameContainingAndUsernameContaining(String firstName, String lastName,
             String username);
 
