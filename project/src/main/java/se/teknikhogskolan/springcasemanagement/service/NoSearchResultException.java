e44d1a1ecfb99d286dd018af317074a4847128f4
peferb <pe.ferb@gmail.com>
2016-11-08 08:58:14 +0100

    NoSerachResultException

---
+++ project/src/main/java/se/teknikhogskolan/springcasemanagement/service/NoSearchResultException.java
@@ -0,0 +1,14 @@
+package se.teknikhogskolan.springcasemanagement.service;
+
+public class NoSearchResultException extends ServiceException {
+
+    private static final long serialVersionUID = 1111170353744704876L;
+
+    protected NoSearchResultException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    protected NoSearchResultException(String message) {
+        super(message);
+    }
+}
