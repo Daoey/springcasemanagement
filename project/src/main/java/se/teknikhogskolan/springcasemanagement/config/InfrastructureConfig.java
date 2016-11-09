10b9a4d71fedf2aae6707140601f7a9b62191cf9
Martin Söderstrand <martinsoderstrand96@hotmail.com>
2016-11-09 13:26:37 +0100

    Implemented Auditing: Creation and Last updated date.

--- project/src/main/java/se/teknikhogskolan/springcasemanagement/config/InfrastructureConfig.java
+++ project/src/main/java/se/teknikhogskolan/springcasemanagement/config/InfrastructureConfig.java
@@ -8,6 +8,7 @@ import com.zaxxer.hikari.HikariDataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
@@ -19,6 +20,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @EnableJpaRepositories("se.teknikhogskolan.springcasemanagement.repository")
 @EnableTransactionManagement
+@EnableJpaAuditing
 public class InfrastructureConfig {
     @Bean
     public DataSource dataSource() {
--- project/src/main/java/se/teknikhogskolan/springcasemanagement/model/AbstractEntity.java
+++ project/src/main/java/se/teknikhogskolan/springcasemanagement/model/AbstractEntity.java
@@ -1,20 +1,41 @@
 package se.teknikhogskolan.springcasemanagement.model;
 
+import java.util.Date;
+
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class AbstractEntity {
     @Id
     @GeneratedValue
     private Long id;
 
+    @CreatedDate
+    private Date created;
+
+    @LastModifiedDate
+    private Date lastModified;
+
     public Long getId() {
         return id;
+    }
+
+    public Date getCreatedDate() {
+        return created;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModified;
     }
 
     @Override
