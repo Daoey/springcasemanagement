6ee1a42864f5e24ace7769e63aa4aaa06300e062
Martin Söderstrand <martinsoderstrand96@hotmail.com>
2016-11-09 12:23:30 +0100

    tests for Issue and Team created

--- project/src/test/java/se/teknikhogskolan/springcasemanagement/model/TestIssue.java
+++ project/src/test/java/se/teknikhogskolan/springcasemanagement/model/TestIssue.java
@@ -1,13 +1,42 @@
 package se.teknikhogskolan.springcasemanagement.model;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.fail;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public final class TestIssue {
 
+    private Issue issue;
+
+    @Before
+    public void setUp() throws Exception {
+        issue = new Issue("description");
+
+    }
+
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void sameTeamShouldBeEqual() {
+        assertEquals(issue, issue);
+    }
+
+    @Test
+    public void differentDescriptionsShouldNotBeEqual() {
+        Issue newIssue = new Issue("diff description");
+        assertNotEquals(newIssue, issue);
+    }
+
+    @Test
+    public void sameDescriptionShouldHaveSameHashCode() {
+        Issue newIssue = new Issue(issue.getDescription());
+        assertEquals(newIssue.hashCode(), issue.hashCode());
+    }
+
+    @Test
+    public void differentObjectTypeShouldNotBeEqual() {
+        String issueString = issue.getDescription();
+        assertNotEquals(issueString, issue);
     }
 }
--- project/src/test/java/se/teknikhogskolan/springcasemanagement/model/TestTeam.java
+++ project/src/test/java/se/teknikhogskolan/springcasemanagement/model/TestTeam.java
@@ -1,13 +1,40 @@
 package se.teknikhogskolan.springcasemanagement.model;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public final class TestTeam {
 
+    private Team team;
+
+    @Before
+    public void setUp() throws Exception {
+        team = new Team("name");
+    }
+
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void sameTeamShouldBeEqual() {
+        assertEquals(team, team);
+    }
+
+    @Test
+    public void differentNamesShouldNotBeEqual() {
+        Team newTeam = new Team("diff name");
+        assertNotEquals(team, newTeam);
+    }
+
+    @Test
+    public void sameNameShouldHaveSameHashCode() {
+        Team newTeam = new Team(team.getName());
+        assertEquals(team.hashCode(), newTeam.hashCode());
+    }
+
+    @Test
+    public void differentObjectTypeShouldNotBeEqual() {
+        String teamString = team.getName();
+        assertNotEquals(teamString, team);
     }
 }
