8992dbc79b72fb22435af480091d21c05015ee1c
Martin Söderstrand <martinsoderstrand96@hotmail.com>
2016-11-10 13:13:52 +0100

    Added new config file for in memory db

---
+++ project/src/main/java/se/teknikhogskolan/springcasemanagement/config/HsqlInfrastructureConfig.java
@@ -0,0 +1,53 @@
+package se.teknikhogskolan.springcasemanagement.config;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaRepositories("se.teknikhogskolan.springcasemanagement.repository")
+@EnableTransactionManagement
+@EnableJpaAuditing
+public class HsqlInfrastructureConfig {
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName("org.hsqldb.jdbc.JDBCDriver");
+        config.setJdbcUrl("jdbc:hsqldb:mem:TestSelf");
+        return new HikariDataSource(config);
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory factory) {
+        return new JpaTransactionManager(factory);
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.HSQL);
+        adapter.setGenerateDdl(true);
+        return adapter;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setDataSource(dataSource());
+        factory.setJpaVendorAdapter(jpaVendorAdapter());
+        factory.setPackagesToScan("se.teknikhogskolan.springcasemanagement.model");
+        return factory;
+    }
+}
