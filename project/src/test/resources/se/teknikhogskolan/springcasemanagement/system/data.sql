969823b0a19e2ebd80cf198abc9e31e16c734a39
Johan Åsling <johan.asling@hotmail.com>
2016-11-11 14:29:57 +0100

    Inserted a user

--- project/src/test/resources/se/teknikhogskolan/springcasemanagement/system/data.sql
+++ project/src/test/resources/se/teknikhogskolan/springcasemanagement/system/data.sql
@@ -1,1 +1,4 @@
-INSERT INTO Team (id, created, active, name) VALUES (1, now, true, 'test');
+INSERT INTO Team (id, created, active, name) VALUES (1, '2016-11-11', true, 'test');
+INSERT INTO Team (id, created, active, name) VALUES (2, '2016-11-11', true, 'Light side');
+
+INSERT INTO User (created, active, username, firstName, lastName) VALUES ('2016-11-11', true, 'Robotarm Luke', 'Luke', 'Skywalker');
