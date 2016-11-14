eaa14e9cbd62098e49dd5f6166715170721c3694
Johan Åsling <johan.asling@hotmail.com>
2016-11-14 11:58:11 +0100

    Renamed test files

--- project/src/test/java/se/teknikhogskolan/springcasemanagement/model/AllModelTests.java
+++ project/src/test/java/se/teknikhogskolan/springcasemanagement/model/AllModelTests.java
@@ -5,5 +5,5 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ TestIssueEntity.class, TestTeamEntity.class, TestUser.class, TestWorkItem.class })
+@SuiteClasses({ TestIssueEntity.class, TestTeamEntity.class, TestUserEntity.class, TestWorkItem.class })
 public class AllModelTests {}
