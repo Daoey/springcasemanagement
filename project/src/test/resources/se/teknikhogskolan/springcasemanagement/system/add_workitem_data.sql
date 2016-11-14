f0dab19b4103b68f905a99ab436260f5ed8a0246
peferb <pe.ferb@gmail.com>
2016-11-14 11:03:16 +0100

    More data for WorkItem integration tests

--- project/src/test/resources/se/teknikhogskolan/springcasemanagement/system/add_workitem_data.sql
+++ project/src/test/resources/se/teknikhogskolan/springcasemanagement/system/add_workitem_data.sql
@@ -1,1 +1,18 @@
-insert into WorkItem (id, created, description) values (98486464, '2016-11-11', 'Do this!');
+insert into Team (id, name, active) values (2465878, 'Teenage Mutant Ninja Turtles', true);
+
+insert into User (id, username, active, team_id, usernumber) values (22523, 'Raphael', true, 2465878, 124234);
+insert into User (id, username, active, team_id, usernumber) values (26344, 'Leonardo', true, 2465878, 124224);
+insert into User (id, username, active, team_id, usernumber) values (25255, 'Donatello', true, 2465878, 924234);
+insert into User (id, username, active, team_id, usernumber) values (23523, 'Michaelangelo', true, 2465878, 184234);
+insert into User (id, username, active) values (10001, 'Splinter', true);
+
+insert into Issue (id, active, description) values (123541, true, 'The turtles need pizza for energy to fight');
+
+insert into WorkItem (id, created, description, status, user_id) 
+	values (98486464, '2016-11-11', 'Lead the team in battle', 'UNSTARTED', 26344);
+	
+insert into WorkItem (id, created, description, status, user_id) 
+	values (45634545, '2016-11-11', 'Keep everybody smiling', 'STARTED', 23523);
+	
+insert into WorkItem (id, created, description, status, user_id, issue_id) 
+	values (12343456, '2016-11-11', 'Lead TMNT', 'STARTED', 10001, 123541);
--- project/src/test/resources/se/teknikhogskolan/springcasemanagement/system/remove_workitem_data.sql
+++ project/src/test/resources/se/teknikhogskolan/springcasemanagement/system/remove_workitem_data.sql
@@ -1,1 +1,13 @@
-delete from WorkItem where description = 'Do this!';
+delete from WorkItem where id = 98486464;
+delete from WorkItem where id = 45634545;
+delete from WorkItem where id = 12343456;
+delete from User where id = 10001;
+delete from User where id = 22523;
+delete from User where id = 26344;
+delete from User where id = 25255;
+delete from User where id = 23523;
+delete from Team where id = 2465878;
+delete from Issue where id = 123541;
+
+--Remove data from tests
+truncate table WorkItem;
