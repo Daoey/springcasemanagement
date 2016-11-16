7164b4231c664e78af20bd904fd0366db5a8a3e2
Martin Söderstrand <martinsoderstrand96@hotmail.com>
2016-11-16 09:30:52 +0100

    Updated exceptions in IssueService

--- project/src/main/java/se/teknikhogskolan/springcasemanagement/service/IssueService.java
+++ project/src/main/java/se/teknikhogskolan/springcasemanagement/service/IssueService.java
@@ -3,6 +3,7 @@ package se.teknikhogskolan.springcasemanagement.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -23,12 +24,7 @@ public class IssueService {
     }
 
     public Issue getById(Long issueId) {
-        Issue issue;
-        try {
-            issue = issueRepository.findOne(issueId);
-        } catch (Exception e) {
-            throw new ServiceException("Could not get issue with id: " + issueId, e);
-        }
+        Issue issue = findIssue(issueId);
 
         if (issue != null) {
             return issue;
@@ -40,8 +36,8 @@ public class IssueService {
         List<Issue> issue;
         try {
             issue = issueRepository.findByDescription(description);
-        } catch (Exception e) {
-            throw new ServiceException("Could not get issues with description: " + description, e);
+        } catch (DataAccessException e) {
+            throw new DatabaseException("Could not get issues with description: " + description, e);
         }
 
         if (issue != null) {
@@ -51,48 +47,40 @@ public class IssueService {
     }
 
     public Issue updateDescription(Long issueId, String description) {
-        try {
-            Issue issue = issueRepository.findOne(issueId);
+        Issue issue = findIssue(issueId);
+        if (issue != null) {
             if (issue.isActive()) {
                 issue.setDescription(description);
-                return issueRepository.save(issue);
+                return saveIssue(issue, "Could not update description on issue with id: " + issueId);
             } else {
-                throw new ServiceException("Could not update "
+                throw new ForbiddenOperationException("Could not update "
                         + "description on Issue with id '" + issueId + "' since it's inactivate.");
             }
-        } catch (ServiceException e) {
-            throw e;
-        } catch (NullPointerException e) {
+        } else {
             throw new NoSearchResultException("Failed to update issue with id '"
-                    + issueId + "' since it could not be found in the database", e);
-        } catch (Exception e) {
-            throw new ServiceException("Could not update description on issue with id: " + issueId, e);
+                    + issueId + "' since it could not be found in the database");
         }
     }
 
     public Issue inactivate(Long issueId) {
-        try {
-            Issue issue = issueRepository.findOne(issueId);
+        Issue issue = findIssue(issueId);
+        if (issue != null) {
             issue.setActive(false);
-            return issueRepository.save(issue);
-        } catch (NullPointerException e) {
+            return saveIssue(issue, "Could not inactivate issue with id: " + issueId);
+        } else {
             throw new NoSearchResultException("Failed to inactivate issue with id '"
-                    + issueId + "' since it could not be found in the database", e);
-        } catch (Exception e) {
-            throw new ServiceException("Could not inactivate issue with id: " + issueId, e);
+                    + issueId + "' since it could not be found in the database");
         }
     }
 
     public Issue activate(Long issueId) {
-        try {
-            Issue issue = issueRepository.findOne(issueId);
+        Issue issue = findIssue(issueId);
+        if (issue != null) {
             issue.setActive(true);
-            return issueRepository.save(issue);
-        } catch (NullPointerException e) {
+            return saveIssue(issue, "Could not activate issue with id: " + issueId);
+        } else {
             throw new NoSearchResultException("Failed to activate issue with id '"
                     + issueId + "' since it could not be found in the database");
-        } catch (Exception e) {
-            throw new ServiceException("Could not activate issue with id: " + issueId, e);
         }
     }
 
@@ -100,13 +88,29 @@ public class IssueService {
         Page<Issue> page;
         try {
             page = pagingIssueRepository.findAll(new PageRequest(pageNumber, pageSize));
-        } catch (Exception e) {
-            throw new ServiceException("Could not get issues by page", e);
+        } catch (DataAccessException e) {
+            throw new DatabaseException("Could not get issues by page", e);
         }
 
         if (page != null) {
             return page;
         } else
             throw new NoSearchResultException("No issues on page: " + pageNumber);
+    }
+
+    private Issue saveIssue(Issue issue, String exceptionMessage) {
+        try {
+            return issueRepository.save(issue);
+        } catch (DataAccessException e) {
+            throw new DatabaseException(exceptionMessage, e);
+        }
+    }
+
+    private Issue findIssue(Long issueId) {
+        try {
+            return issueRepository.findOne(issueId);
+        } catch (DataAccessException e) {
+            throw new DatabaseException("Could not find issue with id: " + issueId, e);
+        }
     }
 }
